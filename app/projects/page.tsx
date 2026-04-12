e20cdd9016f7d61378159fd108869d4589692992
MahmoudNageeb <mahmoudnageeb2709@gmail.com>
2026-04-12 16:09:52 -0700

    Update page.tsx

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -139,9 +139,9 @@ export default function ProjectsPage() {
         </div>
 
         <div className="sbar">
-          <div className="sbar-item anim" style={{ transitionDelay: ".3s" }}><span className="sbar-n">+{totalProjects}</span><span className="sbar-l">مشروع منجز</span></div>
+          <div className="sbar-item anim" style={{ transitionDelay: ".3s" }}><span className="sbar-n">+25</span><span className="sbar-l">مشروع منجز</span></div>
           <div className="sbar-item anim" style={{ transitionDelay: ".4s" }}><span className="sbar-n">{uniqueMinistries}</span><span className="sbar-l">وزارات وجهات</span></div>
-          <div className="sbar-item anim" style={{ transitionDelay: ".5s" }}><span className="sbar-n">{uniqueAreas}</span><span className="sbar-l">مناطق</span></div>
+          <div className="sbar-item anim" style={{ transitionDelay: ".5s" }}><span className="sbar-n">+20</span><span className="sbar-l">مناطق</span></div>
         </div>
       </div>
     </div>
