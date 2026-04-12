f7a83fd0c94905225ab954fd64b4e8610013875e
MahmoudNageeb <mahmoudnageeb2709@gmail.com>
2026-04-12 14:05:19 -0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -87,7 +87,7 @@ export default function Home() {
                 quality={80}
               />
             </div>
-            <span className="pc-label">TAHA NAGEEB</span>
+            <span className="pc-label">ADVACC</span>
           </div>
         </div>
       </div>
